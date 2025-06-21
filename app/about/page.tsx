08e312dd3faf7352b07ae5869cc6999c581bea5a
Bong <borobong230@gmail.com>
2025-06-21 14:28:42 +0900

    :hammer: [Update] about 페이지 설명 변경 #MAIN

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,11 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({
+  title: 'About',
+  description:
+    '블로그 운영자 소개 - 개발자 봉의 경력, 기술 스택, 관심사를 확인해보세요. 웹 개발과 기술 트렌드에 대한 인사이트를 공유합니다.',
+})
 
 export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
